ecbf9ba3c8b2bc08a190bc663bd3b3c5114f2987
davidpierce24 <pie2ce@gmail.com>
2022-04-22 17:44:52 -0400

    loop challenge

---
+++ w1/d5/loopchallenges/script.js
@@ -0,0 +1,39 @@
+// loop 1
+for(var i=1; i<20; i++) {
+    if(i%2 == 1) {
+        console.log(i);
+    }
+}
+
+
+// Loop 2
+for(var i=100; i>=0; i--) {
+    if(i%3 == 0) {
+        console.log(i);
+    }
+}
+
+
+// Loop3
+for(var i=4; i>=-3.5; i-=1.5) {
+    console.log(i);
+}
+
+
+// Loop 4
+var sum = 0;
+
+for(var i=1; i<=100; i++) {
+    sum += i;
+}
+
+console.log(sum);
+
+
+// Loop 5
+var product = 1;
+
+for (var i=2; i<=12; i++) {
+    product *= i;
+}
+console.log(product);
