ec1cb410fd0e1460d65b16d8e1a9a9f4d2aace96
davidpierce24 <pie2ce@gmail.com>
2022-04-22 12:49:04 -0400

    javascript

---
+++ w1/d5/lesson/script.js
@@ -0,0 +1,11 @@
+function disappear (element) {
+    element.remove();
+}
+
+function over(element) {
+    element.style.color = 'lime'
+}
+
+function out(element) {
+    element.style.color = 'black'
+}
