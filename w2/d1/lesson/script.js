35b29c3381403d842b83e10430c8ff7aa6d6f1bf
davidpierce24 <pie2ce@gmail.com>
2022-04-25 12:27:45 -0400

    lesson

---
+++ w2/d1/lesson/script.js
@@ -0,0 +1,25 @@
+var rating1 = [
+    {'name': 'Naruto', 'rating': 'The show where it all began'},
+    {'name': 'Bleach', 'rating': 'Also the show where it all began'},
+    {'name': 'Tokyo Ghoul', 'rating': 'The show where it all began'},
+    {'name': 'Attack on Titan', 'rating': 'The show where it all began'},
+    {'name': 'Demon Slayer', 'rating': 'The show where it all began'},
+    {'name': 'Jujutsu Kaisen', 'rating': 'The show where it all began'},
+    {'name': 'Mob Psycho 100', 'rating': 'The show where it all began'}
+]
+
+
+var anime = [
+    {'name': 'Naruto', 'mc': 'Naruto Uzumaki', 'rating': 9, 'reason': rating1[0].rating},
+    {'name': 'Bleach', 'mc': 'Ichigo Kurosaki', 'rating': 9, 'reason': rating1[1].rating},
+    {'name': 'Tokyo Ghoul', 'mc': 'Ken Kaneki', 'rating': 9, 'reason': rating1[2].rating},
+    {'name': 'Attack on Titan', 'mc': 'Erin Yaeger', 'rating': 9.5, 'reason': rating1[3].rating},
+    {'name': 'Demon Slayer', 'mc': 'Tanjiro Kamado', 'rating': 8, 'reason': rating1[4].rating},
+    {'name': 'Jujutsu Kaisen', 'mc': 'Yuji Itadori', 'rating': 8.5, 'reason': rating1[5].rating},
+    {'name': 'Mob Psycho 100', 'mc': 'Shigeo Kageyama aka Mob', 'rating': 9, 'reason': rating1[6].rating},
+]
+
+
+
+console.log(anime[0]);
+
