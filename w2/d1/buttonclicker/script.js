33d16e3668f1a76c29fdbbf36bbd4efb71f5c2e3
davidpierce24 <pie2ce@gmail.com>
2022-04-25 01:50:07 -0400

    updates

---
+++ w2/d1/buttonclicker/script.js
@@ -0,0 +1,11 @@
+function logout (element) {
+    element.innerText = "Logout";
+}
+
+function over(element) {
+    element.style.opacity = ".8";
+}
+
+function out(element) {
+    element.style.opacity = "100%";
+}
