7e148c4d3a1113d87c581bc0c3ec0b4603898a03
davidpierce24 <pie2ce@gmail.com>
2022-04-26 02:32:01 -0400

    likes

---
+++ w2/d2/Likes/script.js
@@ -0,0 +1,20 @@
+var like1 = document.querySelector('.like1')
+
+var like2 = document.querySelector('.like2')
+
+var like3 = document.querySelector('.like3')
+
+function uptic1() {
+    like1.innerText = 9 + ' likes';
+    like1.innerText 
+}
+
+function uptic2() {
+    like1.innerText = 12 + ' likes';
+    like1.innerText 
+}
+
+function uptic3() {
+    like1.innerText = 9 + ' likes';
+    like1.innerText 
+}
