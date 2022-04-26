650500cfd2a8f37ce54b2d4b08325a74dd6ea58d
davidpierce24 <pie2ce@gmail.com>
2022-04-26 02:43:50 -0400

    updates

--- w2/d2/Likes/script.js
+++ w2/d2/Likes/script.js
@@ -4,17 +4,23 @@ var like2 = document.querySelector('.like2')
 
 var like3 = document.querySelector('.like3')
 
+var like11 = 9;
+
+var like22 = 12;
+
+var like33 = 9;
+
 function uptic1() {
-    like1.innerText = 9 + ' likes';
-    like1.innerText 
+    like1.innerText = like11 + ' likes';
+    like11 ++;
 }
 
 function uptic2() {
-    like1.innerText = 12 + ' likes';
-    like1.innerText 
+    like2.innerText = like22 + ' likes';
+    like22 ++;
 }
 
 function uptic3() {
-    like1.innerText = 9 + ' likes';
-    like1.innerText 
+    like3.innerText = like33 + ' likes';
+    like33 ++;
 }
