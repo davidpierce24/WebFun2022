1c343fa310d766048d759249c5b11a15d1a56157
davidpierce24 <pie2ce@gmail.com>
2022-04-29 10:06:34 -0400

    updates

--- w2/d4/arrayChallenge.js
+++ w2/d4/arrayChallenge.js
@@ -36,10 +36,21 @@ console.log(result); // we expect back [6, 8, 10, 9]
 // Better than average
 function betterThanAverage(arr) {
     var sum = 0;
-    // calculate the average
+    for(var i=0; i<arr.length; i++) {
+        sum += arr[i];
+    }
+    var avg = sum / arr.length;
     var count = 0
-    // count how many values are greated than the average
+   for(var j=0; j<arr.length; j++) {
+       if(arr[j] > avg) {
+           count ++;
+       }
+   }
     return count;
 }
 var result = betterThanAverage([6, 8, 3, 10, -2, 5, 9]);
 console.log(result); // we expect back 4
+
+
+
+// Array Reverse
