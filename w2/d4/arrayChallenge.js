3e1171d582c6684718f7b2f30ee10e562f580942
davidpierce24 <pie2ce@gmail.com>
2022-04-29 16:46:53 -0400

    Array challenge

--- w2/d4/arrayChallenge.js
+++ w2/d4/arrayChallenge.js
@@ -54,3 +54,29 @@ console.log(result); // we expect back 4
 
 
 // Array Reverse
+function reverse(arr) {
+    for(var i=0; i<=(arr.length/2); i++){
+        var temp = arr[arr.length-1-i]
+        arr[arr.length-1-i] = arr[i]
+        arr[i] = temp
+    }
+    return arr;
+}
+   
+var result = reverse(["a", "b", "c", "d", "e"]);
+console.log(result); // we expect back ["e", "d", "c", "b", "a"]
+
+
+
+// Fibonacci Array
+function fibonacciArray(n) {
+    // the [0, 1] are the starting values of the array to calculate the rest from
+    var fibArr = [0, 1];
+    for(var i=1; i<n-1; i++) {
+        fibArr.push((fibArr[i] + fibArr[i-1]));
+    }
+    return fibArr;
+}
+   
+var result = fibonacciArray(10);
+console.log(result); // we expect back [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
