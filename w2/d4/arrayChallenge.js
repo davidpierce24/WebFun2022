3de3d23a805a3e28b1b2996d0870cce9b63d3f72
davidpierce24 <pie2ce@gmail.com>
2022-04-28 21:40:20 -0400

    updates

---
+++ w2/d4/arrayChallenge.js
@@ -0,0 +1,45 @@
+// Always Hungry
+function alwaysHungry(arr) {
+    for (var i=0; i<arr.length; i++){
+        var count = 0;
+        if (arr[i] == 'food') {
+            console.log ('yummy');
+            count ++;
+        }
+    }
+    if (count == 0) {
+        console.log ("I'm hungry");
+    }
+}
+   
+alwaysHungry([3.14, "food", "pie", true, "food"]);
+// this should console log "yummy", "yummy"
+alwaysHungry([4, 1, 5, 7, 2]);
+// this should console log "I'm hungry"
+
+
+// High Pass Filter
+function highPass(arr, cutoff) {
+    var filteredArr = [];
+    for (var i=0; i<arr.length; i++) {
+        if (arr[i] > cutoff) {
+            filteredArr.push(arr[i]);
+        }
+    }
+    return filteredArr;
+}
+var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
+console.log(result); // we expect back [6, 8, 10, 9]
+
+
+
+// Better than average
+function betterThanAverage(arr) {
+    var sum = 0;
+    // calculate the average
+    var count = 0
+    // count how many values are greated than the average
+    return count;
+}
+var result = betterThanAverage([6, 8, 3, 10, -2, 5, 9]);
+console.log(result); // we expect back 4
