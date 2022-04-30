f8257936497dc348be25787ee81e710d7afb72ee
davidpierce24 <pie2ce@gmail.com>
2022-04-30 15:03:42 -0400

    updates

--- w2/d5/calculator/script.js
+++ w2/d5/calculator/script.js
@@ -1,6 +1,12 @@
 var displayDiv = document.querySelector("#display");
 
 function press(num) {
-    displayDiv.innerText = '';
+    // displayDiv.innerText = '';
     displayDiv.innerText += num;
 }
+
+function clr() {
+    displayDiv.innerText = 0;
+}
+
+function setOP()
