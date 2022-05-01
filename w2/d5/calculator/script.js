9e4c4ed10c621e854a3dd1db6b372c0ef9d8b8e5
davidpierce24 <pie2ce@gmail.com>
2022-04-30 20:27:35 -0400

    calculator updates

--- w2/d5/calculator/script.js
+++ w2/d5/calculator/script.js
@@ -1,12 +1,44 @@
 var displayDiv = document.querySelector("#display");
 
 function press(num) {
-    // displayDiv.innerText = '';
+    if(displayDiv.innerText == 0) {
+        displayDiv.innerText = '';
+    }
     displayDiv.innerText += num;
 }
 
 function clr() {
-    displayDiv.innerText = 0;
+    location.reload();
 }
 
-function setOP()
+var keep;
+
+var op;
+
+function setOP(input) {
+    keep = displayDiv.innerText;
+    console.log(keep);
+    op = input;
+    // console.log(displayDiv.innerText);
+    displayDiv.innerText = 0;
+    return keep;
+}
+
+function calculate() {
+    var add = displayDiv.innerText;
+    add = parseInt(add);
+    keep = parseInt(keep);
+    if(op =='+') {
+        var answer = keep + add;
+    }
+    else if (op == '/') {
+        var answer = keep / add;
+    }
+    else if (op == '*') {
+        var answer = keep * add;
+    }
+    else if (op == '-') {
+        var answer = keep * add;
+    }
+    displayDiv.innerText = answer;
+}
